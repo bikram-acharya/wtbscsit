e945022774c3b5d398829bb70b0ee0ce643f3484
Bikram Acharya <bacharyalive@gmail.com>
2022-04-02 21:21:40 +0545

    XML Example

--- js-calculator/main.js
+++ js-calculator/main.js
@@ -1,19 +1,24 @@
-function f(value){
+let plus_minus =false
+function logic(value){
     
     if (value=='clr'){
         document.getElementById("result").value="";
+        result = 0;
+        console.log("From Clr", result)
         return;
     }
    
     else if (result==0 && value==0){
+        console.log("From Zero", result)
         return;
     }
 
     else if(value=='+' || value=='-' || value=='*' || value=='/'){
         operand = value;
         document.getElementById('result').value=value;
-        console.log("from sign:",result)
-        
+        console.log("From Symbol", result)
+        plus_minus =false
+
         return;
     }
     if (value=="="){
@@ -28,6 +33,7 @@ function f(value){
         }
         document.getElementById('result').value = total;
         result=parseFloat(document.getElementById('result').value);
+        plus_minus =false
         return;
     }
 
@@ -35,18 +41,26 @@ function f(value){
         
         document.getElementById('result').value*=-1;
         result=parseFloat(document.getElementById('result').value);
+        plus_minus = true
+        console.log("From Zero", result)
         return;
     }
 
     if(!isNaN(document.getElementById('result').value)){
-        
         document.getElementById('result').value+=value;
-        result = parseFloat(document.getElementById("result").value);
-        console.log("From Number",result)
+        if(plus_minus){
+        result=parseFloat(document.getElementById("result").value)
+        console.log("From inside Num", result,plus_minus)
+        return;
+        }
+        result=parseFloat(document.getElementById("result").value)
+        console.log("From Num", result,plus_minus)
         return;
     }
     else
         document.getElementById('result').value=value;
+
+        console.log("From Non Num", result,plus_minus)
         return;
     
         
